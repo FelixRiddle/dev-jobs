d90a57c4e8403295454a95148c5115f56fe1c869
Felix <fedecraft3312@gmail.com>
2024-06-19 09:30:55 -0300

    Setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import "./globals.css";
+
+/**
+ * Root layout
+ * 
+ * @param param0 
+ * @returns 
+ */
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body>
+                <main>
+                    {children}
+                </main>
+            </body>
+        </html>
+    );
+}
